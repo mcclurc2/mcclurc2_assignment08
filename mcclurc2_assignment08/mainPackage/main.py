c4a518e3a4f4d99f6e36aa8a55a55c0142f5105e
Chase McClure <Chase McClure@DESKTOP-MIJ379N.wifi.uc.edu>
2024-03-21 13:40:07 -0400

    commit

--- mcclurc2_assignment08/mainPackage/main.py
+++ mcclurc2_assignment08/mainPackage/main.py
@@ -1,2 +1,18 @@
 #main.py
 
+import pyodbc
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+                      'Database=IS4010;'
+                      'uid=IS4010Login;'
+                      'pwd=P@ssword2;')
+cursor = conn.cursor()
+# Submit a query to the SQL Server instance and store the results in the cursor object
+cursor.execute('SELECT * FROM tStoreStatus')
+total_fires = 0
+
+for row in cursor:
+    print(row)
+
+
+
